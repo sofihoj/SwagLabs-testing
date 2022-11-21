942d4a94325e6a8873128721bcfc241bc58f45fc
sofihoj <sofiahojberg@gmail.com>
2022-11-21 20:17:26 -0300

    TG-14: improved code

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -6,88 +6,72 @@ describe('Login', () => {
     })
 
     it('Should not login with empty fields', async () => {
-        await LoginPage.login('', '');
-        await invalidLogin('Epic sadface: Username is required')
+        await invalidLogin('', '', 'Epic sadface: Username is required')
     });
 
     it('Should not login with invalid user and empty password', async () =>{
-        await LoginPage.login('invalidUser','');
-        await invalidLogin('Epic sadface: Password is required')
+        await invalidLogin('invalidUser','', 'Epic sadface: Password is required')
     });
 
     it('Should not login with standard_user and empty password', async () => {
-        await LoginPage.login('standard_user', '');
-        await invalidLogin('Epic sadface: Password is required')
+        await invalidLogin('standard_user', '', 'Epic sadface: Password is required')
     });
 
     it('Should not login with standard_user and wrong password', async () => {
-        await LoginPage.login('standard_user', 'test');
-        await invalidLogin('Epic sadface: Username and password do not match any user in this service')
+        await invalidLogin('standard_user', 'test', 'Epic sadface: Username and password do not match any user in this service')
     });
 
     it('Should not login with wrong user and valid password', async () => {
-        await LoginPage.login('test', 'secret_sauce');
-        await invalidLogin('Epic sadface: Username and password do not match any user in this service')
+        await invalidLogin('test', 'secret_sauce', 'Epic sadface: Username and password do not match any user in this service')
     });
 
     it('Should not login with empty user name and correct password', async () => {
-        await LoginPage.login('', 'secret_sauce');
-        await invalidLogin('Epic sadface: Username is required')
+        await invalidLogin('', 'secret_sauce', 'Epic sadface: Username is required')
     });
 
     it('Should not login with locked_out_user and empty password', async () => {
-        await LoginPage.login('locked_out_user', '');
-        await invalidLogin('Epic sadface: Password is required')
+        await invalidLogin('locked_out_user', '', 'Epic sadface: Password is required')
     });
 
     it('Should not login with locked_out_user user and valid password', async () => {
-        await LoginPage.login('locked_out_user', 'secret_sauce');
-        await invalidLogin('Epic sadface: Sorry, this user has been locked out.')
+        await invalidLogin('locked_out_user', 'secret_sauce', 'Epic sadface: Sorry, this user has been locked out.')
     });
 
     it('Should not login with locked_out_user and invalid password', async () => {
-        await LoginPage.login('locked_out_user', 'test');
-        await invalidLogin('Epic sadface: Username and password do not match any user in this service')
+        await invalidLogin('locked_out_user', 'test', 'Epic sadface: Username and password do not match any user in this service')
     });
 
     it('Should not login with problem_user and empty password', async () => {
-        await LoginPage.login('problem_user', '');
-        await invalidLogin('Epic sadface: Password is required')
+        await invalidLogin('problem_user', '', 'Epic sadface: Password is required')
     });
 
     it('Should not login with problem_user and invalid password', async () => {
-        await LoginPage.login('problem_user', 'test');
-        await invalidLogin('Epic sadface: Username and password do not match any user in this service')
+        await invalidLogin('problem_user', 'test', 'Epic sadface: Username and password do not match any user in this service')
     });
 
     it('Should not login with performance_glitch_user and invalid password', async () => {
-        await LoginPage.login('performance_glitch_user', 'test');
-        await invalidLogin('Epic sadface: Username and password do not match any user in this service')
+        await invalidLogin('performance_glitch_user', 'test', 'Epic sadface: Username and password do not match any user in this service')
     });
 
     it('Should not login with performance_glitch_user and empty password', async () => {
-        await LoginPage.login('performance_glitch_user', '');
-        await invalidLogin('Epic sadface: Password is required')
+        await invalidLogin('performance_glitch_user', '', 'Epic sadface: Password is required')
     });
 
     it('Should login with valid credentials (user: standard_user)', async () => {
-        await LoginPage.login('standard_user', 'secret_sauce');
-        await sucessLogin();
+        await sucessLogin('standard_user', 'secret_sauce');
     });
 
     it('Should login with valid credentials (user: problem_user)', async () => {
-        await LoginPage.login('problem_user', 'secret_sauce');
-        await sucessLogin();
+        await sucessLogin('problem_user', 'secret_sauce');
     });
 
     it('Should login with valid credentials (user: performance_glitch_user)', async () => {
-        await LoginPage.login('performance_glitch_user', 'secret_sauce');
-        await sucessLogin();
+        await sucessLogin('performance_glitch_user', 'secret_sauce');
     });
 });
 
-
-let sucessLogin = async () => {
+let sucessLogin = async (username, password) => {
+    await LoginPage.login(username, password);
     await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html');
     await expect(LoginPage.title).toExist();
     await expect(LoginPage.title).toHaveTextContaining('PRODUCTS');
@@ -98,7 +82,8 @@ let sucessLogin = async () => {
     await LoginPage.btnLogout.click();
 }
 
-let invalidLogin = async (message) => {
+let invalidLogin = async (username, password, message) => {
+    await LoginPage.login(username, password);
     await LoginPage.errorMessage.waitForDisplayed();
     await expect(LoginPage.errorMessage).toBeDisplayed();
     await expect(LoginPage.errorMessage).toHaveText(message);
